b4bd4dcaafab5beb23b0d5baf860df76230b863e
Martin Suesskraut <22873554+msuesskraut@users.noreply.github.com>
2019-08-29 22:04:53 +0200

    first real lit test

--- tests/simple_lit.rs
+++ tests/simple_lit.rs
@@ -2,10 +2,13 @@ use rlox_interp::lit::LitTest;
 
 #[test]
 fn simple_lit() {
-    LitTest {
-        cmd: "cargo".into(),
-        args: vec!["run".into()],
-        checks: vec![],
-    }
+    LitTest::default()
+        .cmd("cargo")
+        .args(vec!["run".into()])
+        .checks(vec![
+            "== test ==".into(),
+            "0000   12 OP_CONSTANT      Number(42.0)".into(),
+            "0001    | OP_CONSTANT      Number(23.0)".into(),
+            "0002   13 OP_RETURN".into()])
     .test();
 }
