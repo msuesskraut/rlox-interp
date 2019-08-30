46ccf8749161de28f16dab4812d39935b947a29a
Martin Suesskraut <22873554+msuesskraut@users.noreply.github.com>
2019-08-31 00:54:58 +0200

    moved rlit into separate repo

--- tests/simple_lit.rs
+++ tests/simple_lit.rs
@@ -1,10 +1,13 @@
-use rlox_interp::lit::LitTest;
+use rlit::LitTest;
+
+const CRATE_PATH: &str = env!("CARGO_MANIFEST_DIR");
 
 #[test]
 fn simple_lit() {
     LitTest::default()
         .cmd("cargo")
         .args(vec!["run".into()])
+        .current_dir(CRATE_PATH)
         .checks(vec![
             "== test ==".into(),
             "0000   12 OP_CONSTANT      Number(42.0)".into(),
